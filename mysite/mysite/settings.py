ed7278ac010060c78e90f0d98d3e98a8e4a172ff
jinkazama999 <rizwin.razak1234@gmail.com>
2024-08-29 05:10:55 +0100

    feat(polls): add polls
    added polls

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -20,7 +20,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0#_-*(qmu6#^2jd18m5rk=@net0rjv5is6^8-au%c!z7-bbav$'
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -31,6 +35,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
